e5b7f0844fb6159c7b71e044e162ee7d931ff30e
Jacobwasbeast <jacobwasbeast@outlook.com>
2025-01-16 18:19:40 -0600

    4.1.0 working photoviewer

--- src/Ryujinx.HLE/HOS/Services/Caps/IAlbumAccessorService.cs
+++ src/Ryujinx.HLE/HOS/Services/Caps/IAlbumAccessorService.cs
@@ -17,8 +17,204 @@ namespace Ryujinx.HLE.HOS.Services.Caps
     [Service("caps:a")]
     class IAlbumAccessorService : IpcService
     {
+        public Dictionary<int,string> AlbumFiles { get; set; }
         public IAlbumAccessorService(ServiceCtx context) { }
+
+        public bool IsInitialized = false;
+        [CommandCmif(1)]
+        // GetAlbumFileList() -> (unknown<8>, buffer<unknown, 6>)
+        public ResultCode GetAlbumFileList(ServiceCtx context)
+        {
+            // This gets called twice which causes duplicates of photos so we simply check if it's already initialized.
+            if (!IsInitialized)
+            {
+                IsInitialized = true;
+                return ResultCode.Success;
+            }
+            Logger.Stub?.PrintStub(LogClass.ServiceCaps);
+            string path = AppDataManager.BaseDirPath;
+            path += "\\screenshots";
+            int count = 0;
+            var buffer = context.Request.ReceiveBuff[0];
+            ulong position = buffer.Position;
+            AlbumFiles = new Dictionary<int, string>();
+            Span<AlbumEntry> entries = new AlbumEntry[System.IO.Directory.GetFiles(path).Length];
+            foreach (string file in System.IO.Directory.GetFiles(path))
+            {
+                if (System.IO.Path.GetFileName(file).EndsWith(".png") || System.IO.Path.GetFileName(file).EndsWith(".jpg"))
+                {
+                    AlbumEntry album_entry = new AlbumEntry();
+                    album_entry.EntrySize = (ulong) System.IO.Path.GetFileName(file).Length;
+                    album_entry.FileId = new AlbumFileId();
+                    album_entry.FileId.ApplicationId = 0x0;
+                    album_entry.FileId.Time = FromDateTime(System.IO.File.GetLastWriteTimeUtc(file), (byte)count);
+                    album_entry.FileId.Storage = (byte)AlbumStorage.Sd;
+                    album_entry.FileId.Contents = 0;
+                    album_entry.FileId.Field19_0 = 0;
+                    album_entry.FileId.Field19_1 = 0;
+                    album_entry.FileId.Reserved = 0;
+                    entries[count] = album_entry;
+                    count++;
+                    AlbumFiles.Add(album_entry.FileId.Time.UniqueId, file);
+                }
+            }
+            WriteSpanToBuffer(context, buffer, entries);
+            Logger.Info?.Print(LogClass.ServiceCaps, "$GetAlbumFileCount(): returning " + count);
+            context.ResponseData.Write(count);
+            return ResultCode.Success;
+        }
         
+        [CommandCmif(2)]
+        // LoadAlbumScreenShotImage(unknown<0x18>) -> (unknown<8>, buffer<unknown, 6>)
+        public ResultCode LoadAlbumScreenShotImage(ServiceCtx context)
+        {
+            var fileId = context.RequestData.ReadStruct<AlbumFileId>();
+            return LoadImage(1280, 720, context, fileId);
+        }
+        
+        [CommandCmif(5)]
+        // IsAlbumMounted() -> bool
+        public ResultCode IsAlbumMounted(ServiceCtx context)
+        {
+            // TODO: Implement this properly.
+            Logger.Stub?.PrintStub(LogClass.ServiceCaps);
+            context.ResponseData.Write(true);
+            return ResultCode.Success;
+        }
+        
+       [CommandCmif(8)]
+       // LoadAlbumScreenShotImageEx0
+       public ResultCode LoadAlbumScreenShotImageEx0(ServiceCtx context)
+       {
+           var fileId = context.RequestData.ReadStruct<AlbumFileId>();
+           return LoadImage(1280, 720, context, fileId);
+       }
+       
+       [CommandCmif(14)]
+       public ResultCode LoadAlbumScreenShotThumbnail(ServiceCtx context)
+       {
+           var fileId = context.RequestData.ReadStruct<AlbumFileId>();
+           return LoadImage(320, 180, context, fileId);
+       }
+        
+        public ResultCode LoadImageEx(int width, int height, ServiceCtx context, AlbumFileId fileId)
+        {
+            var outputBuffer = context.Request.ReceiveBuff[0];
+            var inputBuffer = context.Request.ReceiveBuff[1];
+            
+            var output = new LoadAlbumScreenShotImageOutput
+            {
+                width = width,
+                height = height,
+                attribute = new ScreenShotAttribute{
+                    unknown_0 = {},
+                    orientation = AlbumImageOrientation.None,
+                    unknown_1 = {},
+                    unknown_2 = {}
+                }
+            };
+            
+            string imagePath = AlbumFiles[fileId.Time.UniqueId];
+
+            ScaleBytes(width, height, imagePath,  out Span<byte> scaledBytes);
+            
+            ReadOnlySpan<byte> data = MemoryMarshal.Cast<LoadAlbumScreenShotImageOutput, byte>(MemoryMarshal.CreateReadOnlySpan(ref output, 1));
+            byte[] outputBytes = data.ToArray();
+            
+            context.Memory.Write(outputBuffer.Position, outputBytes);
+            context.Memory.Write(inputBuffer.Position, scaledBytes);
+            return ResultCode.Success;
+        }
+        
+        public ResultCode LoadImage(int width, int height, ServiceCtx context, AlbumFileId fileId)
+        {
+            var outputBuffer = context.Request.ReceiveBuff[0];
+            
+            var output = new LoadAlbumScreenShotImageOutput
+            {
+                width = width,
+                height = height,
+                attribute = new ScreenShotAttribute{
+                    unknown_0 = {},
+                    orientation = AlbumImageOrientation.None,
+                    unknown_1 = {},
+                    unknown_2 = {}
+                }
+            };
+            
+            string imagePath = AlbumFiles[fileId.Time.UniqueId];
+            
+            ScaleBytes(width, height, imagePath,  out Span<byte> scaledBytes);
+            
+            ReadOnlySpan<byte> data = MemoryMarshal.Cast<LoadAlbumScreenShotImageOutput, byte>(MemoryMarshal.CreateReadOnlySpan(ref output, 1));
+            byte[] outputBytes = data.ToArray();
+            
+            context.ResponseData.Write(outputBytes);
+            context.Memory.Write(outputBuffer.Position, scaledBytes);
+            return ResultCode.Success;
+        }
+        
+        public ResultCode LoadImageEx1(int width, int height, ServiceCtx context, AlbumFileId fileId)
+        {
+            var outputImageSettings = context.Request.ReceiveBuff[0];
+            var outputData = context.Request.ReceiveBuff[1];
+            var buff3 = context.Request.ReceiveBuff[2];
+            Logger.Info?.Print(LogClass.ServiceCaps, $"Loading thumbnail for {fileId.Time.UniqueId}");
+             
+            var output = new LoadAlbumScreenShotImageOutput
+            {
+                width = width,
+                height = height,
+                attribute = new ScreenShotAttribute{
+                    unknown_0 = {},
+                    orientation = AlbumImageOrientation.None,
+                    unknown_1 = {},
+                    unknown_2 = {}
+                }
+            };
+            
+            string imagePath = AlbumFiles[fileId.Time.UniqueId];
+            
+            ScaleBytes(width, height, imagePath, out Span<byte> scaledBytes);
+            
+            ReadOnlySpan<byte> data = MemoryMarshal.Cast<LoadAlbumScreenShotImageOutput, byte>(MemoryMarshal.CreateReadOnlySpan(ref output, 1));
+            byte[] outputBytes = data.ToArray();
+            
+            context.Memory.Write(outputImageSettings.Position, outputBytes);
+            context.Memory.Write(outputData.Position, scaledBytes);
+            
+            return ResultCode.Success;
+        }
+        
+        public void ScaleBytes(int width, int height, string imagePath, out Span<byte> output_bytes)
+        {
+            using (SKBitmap bitmap = SKBitmap.Decode(imagePath))
+            {
+                if (bitmap == null)
+                    throw new ArgumentException("Unable to decode the input image.");
+
+                // STBI_rgb_alpha
+                SKImageInfo targetInfo = new SKImageInfo(width, height, SKColorType.Rgba8888, SKAlphaType.Premul);
+                using (SKBitmap scaledBitmap = new SKBitmap(targetInfo))
+                {
+                    using (SKCanvas canvas = new SKCanvas(scaledBitmap))
+                    {
+                        canvas.Clear(SKColors.Transparent); // Clear canvas to avoid artifacts
+                
+                        var paint = new SKPaint
+                        {
+                            FilterQuality = SKFilterQuality.High, // High-quality scaling
+                            IsAntialias = true, // Smooth edges
+                        };
+                
+                        // Draw the scaled image
+                        canvas.DrawBitmap(bitmap, new SKRect(0, 0, width, height), paint);
+                    }
+                    
+                    output_bytes = scaledBitmap.Bytes;
+                }
+            }
+        }
         
         [CommandCmif(18)]
         // GetAppletProgramIdTable(buffer<nn::caps::ProgramIdTable>) -> bool
@@ -40,5 +236,107 @@ namespace Ryujinx.HLE.HOS.Services.Caps
             context.ResponseData.Write(false);
             return ResultCode.Success;
         }
+        
+        [CommandCmif(1001)]
+        // LoadAlbumScreenShotThumbnailImageEx0(unknown<0x38>) -> (unknown<0x50>, buffer<unknown, 0x46>, buffer<unknown, 6>)
+        public ResultCode LoadAlbumScreenShotThumbnailImageEx0(ServiceCtx context)
+        {
+            var fileId = context.RequestData.ReadStruct<AlbumFileId>();
+            return LoadImageEx(320, 180, context, fileId);
+        }
+        
+        [CommandCmif(1002)]
+        // LoadAlbumScreenShotImageEx1(unknown<0x38>) -> (buffer<unknown, 0x16>, buffer<unknown, 0x46>, buffer<unknown, 6>)
+        public ResultCode LoadAlbumScreenShotImageEx1(ServiceCtx context)
+        {
+            var fileId = context.RequestData.ReadStruct<AlbumFileId>();
+            
+            return LoadImageEx1(1280, 720, context, fileId);
+        }
+        
+        [CommandCmif(1003)]
+        public ResultCode LoadAlbumScreenShotThumbnailImageEx1(ServiceCtx context)
+        {
+            var fileId = context.RequestData.ReadStruct<AlbumFileId>();
+            return LoadImageEx1(320, 180, context, fileId);
+        }
+
+        public void GetWidthAndHeightFromInputBuffer(AlbumFileId id, out int width, out int height)
+        {
+            string path = AlbumFiles[id.Time.UniqueId];
+            width = 0;
+            height = 0;
+            using (SKBitmap bitmap = SKBitmap.Decode(path))
+            {
+                if (bitmap != null)
+                {
+                    width = bitmap.Width;
+                    height = bitmap.Height;
+                }
+            }
+        }
+
+        public static AlbumFileDateTime FromDateTime(DateTime dateTime, byte id)
+        {
+            return new AlbumFileDateTime
+            {
+                Year = (ushort)dateTime.Year,
+                Month = (byte)dateTime.Month,
+                Day = (byte)dateTime.Day,
+                Hour = (byte)dateTime.Hour,
+                Minute = (byte)dateTime.Minute,
+                Second = (byte)dateTime.Second,
+                UniqueId = id
+            };
+        }
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct AlbumFileId
+    {
+        public ulong ApplicationId;
+        public AlbumFileDateTime Time;
+        public byte Storage;
+        public byte Contents;
+        public byte Field19_0;
+        public byte Field19_1;
+        public uint Reserved;
+    }
+    
+    struct AlbumEntry
+    {
+        public ulong EntrySize;
+        public AlbumFileId FileId;
+    }
+    
+    [StructLayout(LayoutKind.Sequential, Pack = 1,Size = 0x40)]
+    struct ScreenShotAttribute
+    {
+        public uint unknown_0;
+        public AlbumImageOrientation orientation;
+        public uint unknown_1;
+        public uint unknown_2;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 0x500)]
+    struct LoadAlbumScreenShotImageOutput
+    {
+        public long width;
+        public long height;
+        public ScreenShotAttribute attribute;
+    }
+
+    enum AlbumImageOrientation
+    {
+        None,
+        Rotate90,
+        Rotate180,
+        Rotate270
+    }
+
+    struct ApplicationAlbumFileEntry
+    {
+        public ApplicationAlbumEntry ApplicationAlbumEntry;
+        public AlbumFileDateTime Date;
+        public Common.Memory.Array8<byte> Unknown;
     }
 }
